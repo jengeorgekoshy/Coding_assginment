804f8a8834afa8b1cab7585ab278cb987dddbc3b
JEN GEORGE KOSHY <jengeorgekoshy333@gmail.com>
2023-10-25 08:49:13 +0530

    Task 1-A Shuffle array

---
+++ Java/shuffleArray.java
@@ -0,0 +1,21 @@
+// Task 1- A
+// Create an array with the values (1, 2, 3, 4, 5, 6, 7) and shuffle it
+
+import java.util.Random;
+
+public class shuffleArray{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        Random rand = new Random();
+        int arrLength = arr.length;
+        for(int i = arrLength-1;i>0;i--){
+            int r = rand.nextInt(i);
+            int temp = arr[i];
+            arr[i] = arr[r];
+            arr[r] = temp;
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
